a4918f272c849547ba576fc111f704522a6a311d
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-09 01:27:01 +0530

    commit

--- yt_downloader/yt_downloader/settings.py
+++ yt_downloader/yt_downloader/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e27oe-kn^b^krhf_2#hl&^(@13*j^#o6eb4qcnhq(9!&*eis_3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['(.*)']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -122,6 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 import os 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static')
+STATICFILES_DIR = os.path.join(BASE_DIR,'static'),
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
