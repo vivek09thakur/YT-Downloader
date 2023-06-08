2b387f1f941916e753886b68299dcc1bf85ff3b9
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-08 16:10:56 +0530

    commit

--- yt_downloader/downloader/views.py
+++ yt_downloader/downloader/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+# from django.shortcuts import render
+import pytube
+
+def home(request):
+    return render(request, 'index.html')
+
+def download(request):
+    if request.method == 'POST':
+        video_link = request.POST.get('video_link')
+        try:
+            yt = pytube.YouTube(video_link)
+            stream = yt.streams.first()
+            stream.download()
+            return render(request, 'success.html')
+        except Exception as e:
+            error_message = str(e)
+            return render(request, 'error.html', {'error_message': error_message})
+
