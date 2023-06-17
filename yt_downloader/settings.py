d89ae5f2fbab1cbe342c08ace01db62d55d765bd
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-17 18:23:42 +0530

    commit

--- yt_downloader/settings.py
+++ yt_downloader/settings.py
@@ -126,7 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
