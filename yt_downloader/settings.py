e1de3d737581a0f3651371a07e48261f34074bc4
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-17 18:28:39 +0530

    commit

--- yt_downloader/settings.py
+++ yt_downloader/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'yt_downloader.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -125,6 +125,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os 
 
-STATIC_URL = 'static/'
+
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
