8f276aad0416c620ddfa5b36401d2946be7a55a3
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-18 11:26:32 +0530

    commit

--- downloader/views.py
+++ downloader/views.py
@@ -13,18 +13,9 @@ def download(request):
         video_link = request.POST.get('video_link')
         try:
             yt = pytube.YouTube(video_link)
-            stream = yt.streams.filter(progressive=True,
-                                       resolution='720p',
-                                       fps=60,res='max',
-                                       file_extension='mp4'
-                                    ).first()
+            stream = yt.streams.filter().get_highest_resolution()
             stream.download()
-            # file_path = stream.download()
-            # render(request, 'downloader/success.html')
-            # with open(file_path, 'rb') as file:
-            #     response = HttpResponse(file.read(), content_type='video/mp4')
-            #     response['Content-Disposition'] = 'attachment; filename="{0}"'.format(stream.default_filename)
-            #     return response
+            render(request, 'downloader/success.html')
         except Exception as e:
             error_message = str(e)
             return render(request, 'downloader/error.html', {'error_message': error_message})
