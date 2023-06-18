264ce9fe006a633389826c4a53be84449c58dbec
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-18 13:30:10 +0530

    commit

--- downloader/views.py
+++ downloader/views.py
@@ -1,8 +1,7 @@
+from django.http import FileResponse
 from django.http import HttpResponse
 from django.shortcuts import render
-# Create your views here.
 import pytube
-import shutil
 import os
 
 def home(request):
@@ -11,12 +10,16 @@ def home(request):
 def download(request):
     if request.method == 'POST':
         video_link = request.POST.get('video_link')
-        try:
-            yt = pytube.YouTube(video_link)
-            stream = yt.streams.filter().get_highest_resolution()
-            stream.download()
-            render(request, 'downloader/success.html')
-        except Exception as e:
-            error_message = str(e)
-            return render(request, 'downloader/error.html', {'error_message': error_message})
+        yt = pytube.YouTube(video_link)
+        stream = yt.streams.filter().get_highest_resolution()
 
+        if stream:
+            file_path = stream.download()
+            file_name = os.path.basename('./')
+
+            with open(file_path, 'rb') as file:
+                response = HttpResponse(file.read(), content_type='video/mp4')
+                response['Content-Disposition'] = 'attachment; filename="{}"'.format(file_name)
+                return response
+        else:
+            return render(request, 'downloader/error.html')
